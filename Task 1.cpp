1e021175bd0f135e9b430e7aee7786e094a5ee10
ARman0812 <abdullahhere0812@gmail.com>
2024-11-29 16:48:33 +0500

    Update Task 1.cpp

--- Task 1.cpp
+++ Task 1.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int arr[4][4]={ {2,4,6,8},{1,3,6,9},{1,2,3,4},{5,6,7,8} };
+		
+		
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			cout<<arr[i][j]<<"\t";
+		}
+		cout<<"\n";
+	}
+}
